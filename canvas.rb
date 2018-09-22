00b982ed9e49fe3fa8060b80362ca83d41af9d58
Christian Torres <cctorresr@unal.edu.co>
2018-09-22 03:22:19 -0500

    Imprimir tablero

--- canvas.rb
+++ canvas.rb
@@ -1,59 +1,149 @@
-banderaTerminar = false
+require 'matrix'
 
-class Interprete
+class DrawingTool
   def initialize
-    @canvas = Canvas.new()
+    @UInput = UInput.new()
+    @continue = true
   end
 
-  # attr_accessor :archivo
+  # attr_reader :continue
 
-  def leer entrada
-    if entrada != "Q"
-      @entrada = entrada
-    else
-      banderaTerminar = true
+  def run
+    while @continue
+      puts comand = @UInput.read
+      if comand[0] == 'C'
+        @canvas = Canvas.new(comand[1], comand[2])
+      elsif comand[0] == 'Q'
+        continue = false
+      else
+        puts "dibujando"
+        # @canvas.execute comand
+      end
+      @continue = false
     end
   end
+end
 
-  def Validar
+class UInput
+  # def initialize
+  # end
 
+  def read
+    puts "Ingrese comando"
+    entrada = "C 2 2".split(" ")
   end
 
-  def ejecutar
-    if @entrada[0] == "C"
-      @canvas.crearTablero @entrada.split(" ")[1] @entrada.split(" ")[2]
-    end
-
-  end
+  # def validate
+  #
+  # end
 end
 
 class Canvas
-
-  # attr_accessor :archivo
-
-  def crearTablero width height
-    @tablero = " ---
-                |xxx|
-                |   |
-                 ---"
+  def initialize width, height
+    @width = width
+    @height = height
+    createMatrix
+    drawCanvas
   end
 
-  def dibujarLinea
+  # def execute comand
+  #   case comand[0]
+  #   when "L"
+  #     drawLine comand
+  #   when "R"
+  #     drawRectangle comand
+  #   when "B"
+  #     paintArea comand
+  #   else
+  #     puts "Nones"
+  #   end
+  # end
 
+  private
+  # def drawLine
+  #
+  # end
+  #
+  # def drawRectangle
+  #
+  # end
+  #
+  # def paintArea
+  #
+  # end
+
+  def createMatrix
+    @matrix = Array.new(@width.to_i, [])
+    @matrix.each do |column|
+      column = Array.new(@height.to_i, [1])
+    end
+    puts @matrix
   end
 
-  def dibujarRectangulo
-
-  end
-
-  def pintarArea
-
+  def drawCanvas
+    puts "matriz de #{@width} * #{@height}"
+    @matrix.each do |column|
+      # @colum.each do |item|
+        puts item
+      # end
+    end
   end
 end
 
-interprete = Interprete.new()
-interprete.leer
-while banderaTerminar
-  interprete.ejecutar
-  interprete.leer
+puts "Iniciamos"
+drawingTool = DrawingTool.new()
+puts "Corremos"
+# drawingTool.run
+
+# MATRICES
+# puts Matrix.build(3) { rand }
+# matriz = Matrix.build(2, 2) {|row, col| 0 }
+# puts matriz
+# puts matriz[0]
+# matriz.each do |filas|
+#   puts filas
+#   # filas.each do |colum|
+#   #   puts colum
+#   # end
+# end
+
+# ARRAYS
+array = Array.new(2, [" "," "])
+array = Array.new(5, ["X","X"])
+
+#Hacer Matriz
+ancho = 5
+alto = 7
+matriz = Array.new(ancho, [])
+puts matriz.length
+puts matriz[0].class
+for i in (0..alto-1)
+  puts "i: #{i}"
+  # for j in (0..alto-1)
+    # puts "  j: #{j}"
+    matriz[0] << "X"
+    # puts matriz[i][j]
+  # end
+  # puts matriz[i].length
 end
+puts "Matriz: #{matriz}"
+#IMPRIMIR
+filas = alto
+puts filas
+# filas = 5
+# columnas = array.length
+columnas = ancho
+rayas = "--"
+for n in (1..columnas)
+  rayas+="-"
+end
+puts rayas
+for i in(0..filas - 1)
+  # puts "i: #{i}"
+  fila = ""
+  for j in(0..columnas-1)
+    fila += matriz[j][i].to_s
+  end
+  puts "|"+fila+"|"
+end
+puts rayas
